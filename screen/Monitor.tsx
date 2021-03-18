3390dee405aaf6d16e16110554e034f3a91c5bcf
dipsy <h920032@gmail.com>
2021-03-18 22:31:43 +0800

    first commit

--- screen/Monitor.tsx
+++ screen/Monitor.tsx
@@ -5,7 +5,7 @@ import { WebView } from 'react-native-webview';
 function Monitor() {
     return (
       <View style={{height: '100%', width: '100%', flex: 1, alignSelf: 'stretch'}}>
-        <WebView source={{ uri: 'https://www.youtube.com/watch?v=aYe-2Glruu4' }} />
+        <WebView source={{ uri: 'https://youtu.be/Ltg_TERQPsY' }} />
       </View>
     );
 }
